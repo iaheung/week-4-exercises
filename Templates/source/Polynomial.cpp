f0dd43a6f93b15316a07412f0c988f21e7f70fa5
iaheung <ianhyheung@gmail.com>
2025-02-07 15:42:20 +0000

    Implemented Section2 exercise 1

--- Templates/source/Polynomial.cpp
+++ Templates/source/Polynomial.cpp
@@ -1,6 +1,11 @@
 #include "Polynomial.h"
 
-int quadratic(int a, int b, int c, int x)
+template<typename T>
+T quadratic(T a, T b, T c, T x)
 {
     return a*x*x + b*x + c;
 }
+
+template int quadratic(int, int, int, int);
+template float quadratic(float, float, float, float);
+template double quadratic(double, double, double, double);
--- Templates/source/Templates.cpp
+++ Templates/source/Templates.cpp
@@ -6,7 +6,13 @@ int main()
 {
     int n_int = 5;
     int quad_int = quadratic(0, 2, 9, n_int);
+    float quad_float = quadratic(0.0, 2.0, 9.0, 5.1);
+    double quad_double = quadratic(0.0, 2.0, 9.0, 5.1);
+    double d_quad = quadratic<double>(1.4, 2, 3.9, 0.1);
     std::cout << quad_int << std::endl;
+    std::cout << quad_float << std::endl;
+    std::cout << quad_double << std::endl;
+    std::cout << d_quad << std::endl;
 
     return 0;
 }
